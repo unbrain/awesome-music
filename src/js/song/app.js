61a382960f651b286b5244a45c1280fa82a828c6
unbrain <marsorsun@gmail.com>
2018-06-01 16:13:57 +0800

    歌曲页播放功能

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -1,6 +1,25 @@
 
 {
-    let view = {}
+    let view = {
+        el: '#app',
+        template: `
+        <audio src="{{url}}"></audio>
+        <div>
+            <button class="play">play</button>
+            <button class="pause">pause</button>
+        </div>`,
+        render(data){
+            $(this.el).html(this.template.replace('{{url}}', data))
+        },
+        play(){
+            let audio = $(this.el).find('audio')[0]
+            audio.play()
+        },
+        pause(){
+            let audio = $(this.el).find('audio')[0]
+            audio.pause()
+        },
+    }
     let model = {
         data: {
             id: '',
@@ -22,7 +41,16 @@
             this.view = view
             this.model = model
             this.model.setData(this.getId()).then(() => {
-                console.log(this.model.data)
+                this.view.render(this.model.data.url)
+            })
+            this.bindEvents()
+        },
+        bindEvents(){
+            $(this.view.el).on('click', '.play', () => {
+                this.view.play()
+            })
+            $(this.view.el).on('click', '.pause', () => {
+                this.view.pause()
             })
         },
         getId(){
